27a6c1ec19afed09e5c219ba83ff3bfce061a5ed
gonghaima@hotmail.com <5418854188>
2015-11-10 00:23:20 +1300

    Typescript added

---
+++ angularBeer/app/controller/beers.controller.ts
@@ -0,0 +1,56 @@
+﻿module app.beers {
+    'use strict';
+
+    import Beer = app.services.Beer;
+
+    interface IBeersScope {
+        Beers: Beer[];
+        newBeer: Beer;
+        showBeer: Boolean;
+
+        getBeers(): ng.IPromise<Beer[]>;
+        addBeer(newBeer: Beer): ng.IPromise<Beer>;
+        showBeerForm(showBeer: Boolean): void;
+    }
+
+
+
+    class BeersController implements IBeersScope {
+        Beers: Beer[];
+        newBeer: Beer;
+        showBeer: Boolean;
+
+        static $inject = ['$http', 'app.services.BeersService'];
+
+        constructor(private $http: ng.IHttpService, private beersService: services.IBeersService) {
+            this.getBeers();
+            this.showBeer = false;
+        }
+
+        getBeers(): ng.IPromise<Beer[]> {
+            return this.beersService.getBeers()
+                .then((data: ng.IHttpPromiseCallbackArg<Beer[]>): any=> {
+                    this.Beers = <Beer[]>data;
+                });
+
+        };
+
+        addBeer(nb): ng.IPromise<Beer> {
+            return this.beersService.addBeer(nb)
+                .then((response: ng.IHttpPromiseCallbackArg<Beer>): any=> {
+                    this.showBeer = false;
+                    this.Beers.push(nb);
+                    this.newBeer = <any>{};
+                });
+
+        };
+        showBeerForm(showBeer: Boolean): void {
+            this.showBeer = showBeer;
+        };
+    }
+
+    angular
+        .module('app')
+        .controller('app.beers.BeersController', BeersController);
+
+}
---
+++ angularBeer/app/services/beers.service.ts
@@ -0,0 +1,44 @@
+﻿module app.services {
+    'use strict';
+
+    export interface IBeersService {
+        getBeers(): ng.IPromise<Beer[]>;
+        addBeer(newBeer: Beer): ng.IPromise<Beer>;
+    }
+
+    export class Beer {
+        id: string;
+        name: string;
+        colour: string;
+        hasTried: Boolean;
+    }
+
+    class BeersService implements IBeersService {
+        static $inject = ['$http'];
+        constructor(private $http: ng.IHttpService) {
+        }
+        getBeers(): ng.IPromise<Beer[]> {
+            return this.$http.get('/api/Beers')
+                .then((response: ng.IHttpPromiseCallbackArg<Beer[]>): any=> {
+                    return <Beer[]>response.data;
+                });
+        }
+
+        addBeer(newBeer:Beer): ng.IPromise<Beer> {
+            return this.$http.post('/api/Beers', newBeer)
+                .then((response: ng.IHttpPromiseCallbackArg<Beer>): any=> {
+                    <Beer>response.data;
+                });
+        };
+    }
+
+    factory.$inject = ['$http'];
+
+    function factory($http: ng.IHttpService) {
+        return new BeersService($http);
+    }
+
+    angular
+        .module('app')
+        .factory('app.services.BeersService', factory);
+}
